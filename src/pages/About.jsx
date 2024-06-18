0127e629ecc517e8e8a41701da39885bbe390954
Lukas Brandtner <lukas_brandtner@hotmail.com>
2024-06-18 16:51:15 +0200

    about update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,11 @@
 function About() {
   return (
-    <div>
+    <div className='flex items-center justify-center flex-col'>
       <h1 className='text-6xl mb-4'>Github Finder</h1>
-      <p className='mb-4 text-2xl font-light'>
+      <p className='mb-4 text-2xl font-light text-center'>
         A React app to search GitHub profiles and see profile details.
       </p>
-      <p className='text-lg text-gray-400'>
+      <p className='text-lg text-gray-400 text-center'>
         Version <span className='text-white'>1.0.0</span>
       </p>
     </div>
